613a1f6c9c5c109bc3364a8ff155008445ad000e
iredica <iredica@gmail.com>
2026-08-03 09:23:09 +0200

    COMANDO USER terminado y controlado el tema del registro

--- src/Server/ServerCommands.cpp
+++ src/Server/ServerCommands.cpp
@@ -4,11 +4,11 @@ void Server::executeCommand(Client &client, const std::string &command, const st
 {
     if (command == "PASS")
         handlePass(client, args);
-    /*else if (command == "NICK")
+    else if (command == "NICK")
         handleNick(client, args);
     else if (command == "USER")
         handleUser(client, args);
-    else if (command == "JOIN")
+    /*else if (command == "JOIN")
         handleJoin(client, args);
     else if (command == "PART")
         handlePart(client, args);
@@ -38,4 +38,73 @@ void Server::handlePass(Client &client, const std::vector<std::string> &args)
 
     std::cout << "Client " << client.getFd()
               << " authenticated successfully." << std::endl;
+}
+
+static void Server::tryRegister(Client &client)
+{
+    if (client.isPassAccepted() && !client.getNickname().empty() && !client.getUsername().empty() && !client.isRegistered())
+    {
+        client.setRegistered(true);
+        std::cout << "Client " << client.getFd()
+                  << " registered successfully." << std::endl;
+    }
+}
+
+void Server::handleNick(Client &client, const std::vector<std::string> &args)
+{
+    if (!client.isPassAccepted())
+    {
+        sendDataToClient(client.getFd(),
+                         "464 :You must authenticate first\r\n");
+        return;
+    }
+
+    if (args.empty())
+    {
+        sendDataToClient(client.getFd(),
+                         "431 :No nickname given\r\n");
+        return;
+    }
+
+    for (std::map<int, Client>::const_iterator it = _clients.begin(); it != _clients.end(); ++it)
+    {
+        if (it->second.getNickname() == args[0])
+        {
+            sendDataToClient(client.getFd(),
+                             "433 :Nickname is already in use\r\n");
+            return;
+        }
+    }
+
+    client.setNickname(args[0]);
+    std::cout << "Client " << client.getFd()
+              << " set nickname to: " << args[0] << std::endl;
+
+    tryRegister(client);
+}
+
+void Server::handleUser(Client &client, const std::vector<std::string> &args)
+{
+    if (!client.isPassAccepted())
+    {
+        sendDataToClient(client.getFd(),
+                         "464 :You must authenticate first\r\n");
+        return;
+    }
+
+    if (args.size() < 4)
+    {
+        sendDataToClient(client.getFd(),
+                         "461 USER :Not enough parameters\r\n");
+        return;
+    }
+
+    client.setUsername(args[0]);
+    client.setRealname(args[3]);
+
+    std::cout << "Client " << client.getFd()
+              << " registered with username: " << args[0]
+              << " and realname: " << args[3] << std::endl;
+
+    tryRegister(client);
 }
